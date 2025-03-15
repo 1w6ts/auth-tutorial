8e268275ae6a38b799d96e1f9a5e39a93e295f35
jacobrozwadowski <xype.discord@gmail.com>
2025-03-15 10:17:16 +0100

    change comment in db instance

--- src/db/index.ts
+++ src/db/index.ts
@@ -4,4 +4,4 @@ import { config } from "../config/config";
 
 const instance = neon(config.database.url!);
 
-export const db = drizzle(instance as any); // ignore this
+export const db = drizzle(instance as any); // ignore this type plzzz
